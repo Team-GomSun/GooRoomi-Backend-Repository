f448f13ea165270632492d99ee226237d4ddf26b
so1eeee <soljeong821@gmail.com>
2025-04-11 18:59:34 +0900

    init

---
+++ src/main/java/server/gooroomi/ServerApplication.java
@@ -0,0 +1,13 @@
+package server.gooroomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
