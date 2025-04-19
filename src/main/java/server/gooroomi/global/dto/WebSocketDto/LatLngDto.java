950139aa53b9ec388eaee3673fc711fbe508a1da
bingseok <qqq7384@naver.com>
2025-04-19 21:08:56 +0900

    Feat: 위/경도 DTO 생성

---
+++ src/main/java/server/gooroomi/global/dto/WebSocketDto/LatLngDto.java
@@ -0,0 +1,11 @@
+package server.gooroomi.global.dto.WebSocketDto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LatLngDto {
+    private double latitude;
+    private double longitude;
+}
