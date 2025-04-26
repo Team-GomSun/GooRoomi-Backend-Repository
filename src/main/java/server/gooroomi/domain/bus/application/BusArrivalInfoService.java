a097c5910b9a5901c797446a5851c68bba961525
so1eeee <soljeong821@gmail.com>
2025-04-26 21:40:20 +0900

    [feat/#6] 사용자의 위치 기반 가장 가까운 정류소 및 버스 도착 정보 저장 기능 구현

---
+++ src/main/java/server/gooroomi/domain/bus/application/BusArrivalInfoService.java
@@ -0,0 +1,49 @@
+package server.gooroomi.domain.bus.application;
+
+import lombok.RequiredArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+import server.gooroomi.domain.bus.api.BusInfoApiClient;
+import server.gooroomi.domain.bus.entity.BusArrival;
+import server.gooroomi.domain.bus.entity.BusStation;
+import server.gooroomi.domain.bus.repository.BusArrivalRepository;
+import server.gooroomi.domain.bus.repository.BusStationRepository;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Service
+@RequiredArgsConstructor
+public class BusArrivalInfoService {
+
+    private final BusInfoApiClient busInfoApiClient;
+    private final BusArrivalRepository busArrivalRepository;
+    private final BusStationRepository busStationRepository;
+
+    public void saveBusArrivalInfo(String arsId) {
+        String json = busInfoApiClient.getBusArrivals(arsId);
+        BusStation busStation = busStationRepository.findByArsId(arsId);
+        busStation.getBusArrivals().clear();
+
+        JSONObject root = new JSONObject(json);
+        JSONArray itemList = root.getJSONObject("msgBody").optJSONArray("itemList");
+
+        List<BusArrival> busArrivals = IntStream.range(0, itemList.length())
+                .mapToObj(i -> {
+                    JSONObject item = itemList.getJSONObject(i);
+                    String busNumber = item.getString("rtNm");
+                    String arrivalTime = item.getString("traTime1");
+
+                    BusArrival busArrival = BusArrival.builder()
+                            .busNumber(busNumber)
+                            .arrivalTime(arrivalTime)
+                            .build();
+                    busArrival.assignBusStation(busStation);
+                    return busArrival;
+                })
+                .toList();
+
+        busArrivalRepository.saveAll(busArrivals);
+    }
+}
---
+++ src/main/java/server/gooroomi/domain/bus/application/BusStationAssignService.java
@@ -0,0 +1,50 @@
+package server.gooroomi.domain.bus.application;
+
+import lombok.RequiredArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+import server.gooroomi.domain.bus.api.StationInfoApiClient;
+import server.gooroomi.domain.bus.converter.BusConverter;
+import server.gooroomi.domain.bus.entity.BusStation;
+import server.gooroomi.domain.bus.repository.BusStationRepository;
+import server.gooroomi.domain.user.entity.User;
+import server.gooroomi.global.handler.response.BaseException;
+import server.gooroomi.global.handler.response.BaseResponseStatus;
+
+@Service
+@RequiredArgsConstructor
+public class BusStationAssignService {
+
+    private final StationInfoApiClient stationInfoApiClient;
+    private final BusStationRepository busStationRepository;
+    private final BusArrivalInfoService busArrivalInfoService;
+
+    public void saveBusStation(User user) {
+        String json = stationInfoApiClient.getNearbyStations(user.getLongitude(), user.getLatitude(), 100);
+
+        JSONObject root = new JSONObject(json);
+        JSONArray itemList = root.getJSONObject("msgBody").optJSONArray("itemList");
+
+        if(itemList == null || itemList.isEmpty()){
+            throw new BaseException(BaseResponseStatus.NOT_FOUND_STATION);
+        }
+
+        JSONObject nearest = itemList.getJSONObject(0);
+        String arsId = nearest.getString("arsId");
+        String stationNm = nearest.getString("stationNm");
+
+        BusStation existingBusStation = busStationRepository.findByUser(user);
+
+        if (existingBusStation == null) {
+            BusStation newBusStation = BusConverter.toBusStation(arsId, stationNm, user);
+            busStationRepository.save(newBusStation);
+        } else if (!existingBusStation.getArsId().equals(arsId)) {
+            busStationRepository.delete(existingBusStation);
+            BusStation newBusStation = BusConverter.toBusStation(arsId, stationNm, user);
+            busStationRepository.save(newBusStation);
+        }
+
+        busArrivalInfoService.saveBusArrivalInfo(arsId);
+    }
+}
