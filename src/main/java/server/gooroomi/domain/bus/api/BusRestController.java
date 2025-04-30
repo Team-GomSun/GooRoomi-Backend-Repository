b593005559d3cff7f2cd5a942ca07abd0b417fa7
so1eeee <soljeong821@gmail.com>
2025-04-30 17:57:47 +0900

    [feat/#13] 버스 도착 정보 조회 기능 구현

---
+++ src/main/java/server/gooroomi/domain/bus/api/BusRestController.java
@@ -0,0 +1,25 @@
+package server.gooroomi.domain.bus.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import server.gooroomi.domain.bus.application.BusService;
+import server.gooroomi.domain.bus.dto.BusArrivalResponse;
+import server.gooroomi.global.handler.response.BaseResponse;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/bus")
+public class BusRestController {
+
+    private final BusService busService;
+
+    @GetMapping("/arrival")
+    public BaseResponse<List<BusArrivalResponse>> getArrivingBuses(Long userId){
+        List<BusArrivalResponse> arrivingBuses = busService.getArrivingBuses(userId);
+        return BaseResponse.success(arrivingBuses);
+    }
+}
