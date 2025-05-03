553375868fb99ba6c9aa732051b56b74f6729625
so1eeee <soljeong821@gmail.com>
2025-05-03 18:42:35 +0900

    refactor: BusStationRepository에서 불필요한 arsId 조회 메서드 제거

--- src/main/java/server/gooroomi/domain/bus/repository/BusStationRepository.java
+++ src/main/java/server/gooroomi/domain/bus/repository/BusStationRepository.java
@@ -6,5 +6,4 @@ import server.gooroomi.domain.bus.entity.BusStation;
 
 @Repository
 public interface BusStationRepository extends JpaRepository<BusStation, Long> {
-    BusStation findByArsId(String arsId);
 }
