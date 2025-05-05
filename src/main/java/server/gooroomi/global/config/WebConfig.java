a86ddc9a8fc4b37088d9ec82c200b55297aa05fb
so1eeee <soljeong821@gmail.com>
2025-05-05 18:12:22 +0900

    fix: CORS allowed origins 설정 변경

--- src/main/java/server/gooroomi/global/config/WebConfig.java
+++ src/main/java/server/gooroomi/global/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://gooroomi.p-e.kr", "https://7707-210-107-70-178.ngrok-free.app", "https://gooroomi.vercel.app")
+                .allowedOrigins("http://localhost:3000", "https://gooroomi.p-e.kr", "https://7707-210-107-70-178.ngrok-free.app", "https://gooroomi.vercel.app", " https://e5fb-121-131-128-10.ngrok-free.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메소드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true); // 자격 증명 허용
