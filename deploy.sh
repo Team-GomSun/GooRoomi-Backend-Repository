a55a43f8d54ba6c5fc14bc637e8624e18c64675b
so1eeee <soljeong821@gmail.com>
2025-05-07 23:02:18 +0900

    [chore/#19] 배포 스크립트 개선

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,19 @@ else
         sleep 3
 fi
 
-mkdir -p /home/ubuntu/gooroomi/gooroomi-prod-logs
-chmod 755 /home/ubuntu/gooroomi/gooroomi-prod-logs
+# 로그 디렉토리 설정
+LOG_DIR="/home/ubuntu/gooroomi/gooroomi-prod-logs"
+mkdir -p $LOG_DIR
+chown ubuntu:ubuntu $LOG_DIR
+chmod 750 $LOG_DIR
 
+# Docker Compose 실행
+cd /home/ubuntu/gooroomi
+docker-compose down
+docker-compose up -d
+
+# 애플리케이션 실행
 JAR_PATH="/home/ubuntu/gooroomi/gooroomi-0.0.1-SNAPSHOT.jar"
 echo "jar path : $JAR_PATH"
 chmod +x $JAR_PATH
-nohup java -jar $JAR_PATH --spring.profiles.active=prod > /home/ubuntu/gooroomi/gooroomi-prod-logs/application.log 2>&1 &
+nohup java -jar $JAR_PATH --spring.profiles.active=prod &
