3f29fa37f07894a692ceb71e8fa730c9dbca02be
so1eeee <soljeong821@gmail.com>
2025-05-17 20:28:02 +0900

    [refactor/#24] Swagger 설정 정리 및 API 문서 정보 개선

--- src/main/java/server/gooroomi/global/config/SwaggerConfig.java
+++ src/main/java/server/gooroomi/global/config/SwaggerConfig.java
@@ -1,38 +1,32 @@
 package server.gooroomi.global.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
-    public OpenAPI GooroomiAPI() {
-        Info info = new Info()
-                .title("Gooroomi Swagger")
-                .description("Gooroomi server Swagger")
-                .version("1.0.0");
+    public OpenAPI openAPI() {
+        String title = "🚌 구루미 API";
+        String description = """
+                구루미 프로젝트 API 문서입니다.
 
-        String jwtSchemeName = "JWT TOKEN";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
-        Components components = new Components()
-                .addSecuritySchemes("JWT TOKEN", new SecurityScheme()
-                        .name("Authorization")
-                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
-                        .in(SecurityScheme.In.HEADER)
-                        .scheme("bearer")
-                        .bearerFormat("JWT")
-                );
+                ### 구루미 프로젝트
+                구루미 애플리케이션의 백엔드 API입니다.
 
-        return new OpenAPI()
-                .addServersItem(new Server().url("/"))
-                .info(info)
-                .addSecurityItem(securityRequirement)
-                .components(components);
+                ### 응답 코드
+                - 200: 요청 성공
+                - 400: 잘못된 요청
+                - 404: 리소스 없음
+                - 500: 서버 오류
+                """;
+
+        // API 정보 설정
+        Info info = new Info().title(title).description(description).version("v1.0.0");
+
+        return new OpenAPI().info(info);
     }
 }
