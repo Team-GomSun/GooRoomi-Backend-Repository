d16d813565c29bfc3c28796ce6a014ff6278f279
so1eeee <soljeong821@gmail.com>
2025-05-31 06:27:00 +0900

    [fix/#39] docker-compose 경로 상대경로로 수정

--- script/init-deploy.sh
+++ script/init-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 작업 디렉토리를 확인하고 필요한 디렉토리 생성
+# 로그 디렉토리 생성
 mkdir -p /home/ubuntu/gooroomi/gooroomi-prod-logs
 chmod 755 /home/ubuntu/gooroomi/gooroomi-prod-logs
 
@@ -9,8 +9,8 @@ docker network create gooroomi-network || true
 
 # Blue 환경 시작
 echo "Blue 환경을 시작합니다..."
-docker-compose -f /home/ubuntu/gooroomi/docker-compose.blue.yml build
-docker-compose -f /home/ubuntu/gooroomi/docker-compose.blue.yml up -d
+docker-compose -f docker-compose.blue.yml build
+docker-compose -f docker-compose.blue.yml up -d
 
 # Blue 환경이 준비될 때까지 대기
 echo "Blue 환경이 준비될 때까지 10초 대기..."
@@ -24,13 +24,13 @@ if [ "$HEALTH_CHECK" == "200" ]; then
   
   # Green 환경도 시작
   echo "Green 환경을 시작합니다..."
-  docker-compose -f /home/ubuntu/gooroomi/docker-compose.green.yml build
-  docker-compose -f /home/ubuntu/gooroomi/docker-compose.green.yml up -d
+  docker-compose -f docker-compose.green.yml build
+  docker-compose -f docker-compose.green.yml up -d
   
   echo "Blue-Green 배포 환경이 준비되었습니다."
   echo "현재 Blue 환경이 활성화되어 있습니다."
 else
   echo "Blue 환경 Health check 실패! 초기 설정을 중단합니다."
-  docker-compose -f /home/ubuntu/gooroomi/docker-compose.blue.yml down
+  docker-compose -f docker-compose.blue.yml down
   exit 1
 fi 
